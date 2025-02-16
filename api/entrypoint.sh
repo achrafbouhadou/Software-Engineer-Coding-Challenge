9fddc09e04ff89be1648d7934ed4f92a25493304
achrafbouhadou <achraf1323@gmail.com>
2025-02-16 20:21:04 +0100

    FIX : add match query to elastic search search func

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -9,12 +9,16 @@ if ! grep -q '^APP_KEY=' .env; then
   echo "APP_KEY not found, generating one..."
   php artisan key:generate --force
 fi
+echo "Linking storage..."
+php artisan storage:link
 
 echo "Migrating the database..."
 php artisan migrate --force
 
 echo "Setting up Elasticsearch indices..."
 php artisan elasticsearch:setup
+
+
 
 echo "Starting Laravel application..."
 exec php artisan serve --host=0.0.0.0 --port=9000
