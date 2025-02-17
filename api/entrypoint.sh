77ef18603f6e174c7d5aaa50b4f828ebd7b22a9a
achrafbouhadou <achraf1323@gmail.com>
2025-02-17 21:20:57 +0100

    FIX : set permision

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 set -e
 
+echo "Setting permissions..."
+chmod -R 775 /var/www 
+chown -R www-data:www-data /var/www
+
 echo "Running composer install..."
 composer install --no-interaction --prefer-dist --optimize-autoloader
+
 
 # Check if APP_KEY is set in .env; if not, generate one.
 if ! grep -q '^APP_KEY=' .env; then
   echo "APP_KEY not found, generating one..."
   php artisan key:generate --force
 fi
-
 echo "Linking storage..."
 php artisan storage:link
 
@@ -19,11 +23,7 @@ php artisan migrate --force
 echo "Setting up Elasticsearch indices..."
 php artisan elasticsearch:setup
 
-# If a command is provided, run it; otherwise, start Laravel
-if [ "$#" -gt 0 ]; then
-  echo "Running custom command: $@"
-  exec "$@"
-else
-  echo "Starting Laravel application..."
-  exec php artisan serve --host=0.0.0.0 --port=9000
-fi
+
+
+echo "Starting Laravel application..."
+exec php artisan serve --host=0.0.0.0 --port=9000
